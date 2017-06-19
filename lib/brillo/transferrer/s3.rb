d724c7d7c0dfd4cde1bd75256ef08e4483808f3c
Rob Kaufman <rob@notch8.com>
2017-06-19 16:03:45 -0700

    make brillo 1.6 compatible

--- lib/brillo/transferrer/s3.rb
+++ lib/brillo/transferrer/s3.rb
@@ -13,27 +13,58 @@ module Brillo
         @region               = config.transfer_config.region
         @filename             = config.compressed_filename
         @path                 = config.compressed_dump_path
-        Aws.config.update(aws_config(config.transfer_config))
+        if defined? Aws
+          Aws.config.update(aws_config(config.transfer_config))
+        else
+          AWS.config(access_key_id: config.transfer_config.access_key_id, secret_access_key: config.transfer_config.secret_access_key, region: config.transfer_config.region)
+        end
       end
 
       def download
         return unless enabled
         logger.info("download #{path} from s3 #{bucket} #{filename}")
         FileUtils.rm path, force: true
-        client.get_object({bucket: bucket, key: filename}, target: path)
-      rescue Aws::S3::Errors::NoSuchBucket
-        create_bucket
-        retry
+        if defined? Aws
+          begin
+            client.get_object({bucket: bucket, key: filename}, target: path)
+          rescue Aws::S3::Errors::NoSuchBucket
+            create_bucket
+            retry
+          end
+        else
+          begin
+            File.open(path, 'wb') do |file|
+              client.bucket(bucket).objects(filename).read do |chunk|
+                file.write(chunk)
+              end
+            end
+          rescue AWS::S3::Errors::NoSuchBucket
+            create_bucket
+            retry
+          end
+        end
       end
 
       def upload
         return unless enabled
         logger.info("uploading #{path} to s3 #{bucket} #{filename}")
-        object = resource.bucket(bucket).object(filename)
-        object.upload_file(path)
-      rescue Aws::S3::Errors::NoSuchBucket
-        create_bucket
-        retry
+        if defined? Aws
+          begin
+          object = resource.bucket(bucket).object(filename)
+          object.upload_file(path)
+          rescue Aws::S3::Errors::NoSuchBucket 
+            create_bucket
+            retry
+          end
+        else
+          begin
+          object = client.bucket(bucket).objects[filename]
+          object.write(path)
+          rescue AWS::S3::Errors::NoSuchBucket
+            create_bucket
+            retry
+          end
+        end
       end
 
       private
@@ -53,11 +84,19 @@ module Brillo
       end
 
       def create_bucket
-        client.create_bucket(bucket: bucket)
+        if defined? Aws
+          client.create_bucket(bucket: bucket)
+        else
+          client.buckets.create(bucket)
+        end
       end
 
       def client
-        Aws::S3::Client.new
+        if defined? Aws
+          Aws::S3::Client.new
+        else
+          AWS::S3.new
+        end
       end
 
       def resource
